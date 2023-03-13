ec0d9cdac7fab54af8a0f040e48a31d745b34aed
La-Lucia <laura.umana2012@gmail.com>
2023-03-13 17:44:25 -0600

    Add project files.

---
+++ GrabarVideo/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace GrabarVideo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage()) { BarBackgroundColor = Color.FromHex("#6495ED") };
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ GrabarVideo/MainPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using Plugin.Media;
+using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration;
+
+namespace GrabarVideo
+{
+    public partial class MainPage : ContentPage
+    {
+
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            try {
+
+                var opciones = await CrossMedia.Current.TakeVideoAsync(new Plugin.Media.Abstractions.StoreVideoOptions
+                {
+                    Directory = "Videos",
+                    Name ="VID_" + DateTime.Now.ToString() + ".mp4",
+                    SaveToAlbum = true
+                });
+
+
+                if (opciones == null)
+                {
+                    await DisplayAlert("Atencion", "Archivo Vacio", "Aceptar");
+                    return;
+                }else
+                {
+                    var path = opciones.Path;
+                    await DisplayAlert("Archivo guardado", "El video se ha guardado en la ruta:  " + path, "Aceptar");
+                }
+
+                
+
+
+
+
+            } catch (Exception ex) {
+
+                await DisplayAlert("Error", "Error" + ex, "Aceptar");
+            
+            }
+
+
+    }
+
+        private void VideoPlayer_PlayCompletion(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
